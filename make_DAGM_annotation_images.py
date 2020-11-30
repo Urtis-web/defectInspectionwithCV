7708a48c61e20060524a268ab0927f0eeda66566
Urtis <urtishuhu@gmail.com>
2020-12-01 01:14:41 +0200

    Nesitikejau, bet pavyko :)

--- make_DAGM_annotation_images.py
+++ make_DAGM_annotation_images.py
@@ -4,7 +4,10 @@ import cv2
 import numpy as np
 import math
 
+
 # self-explanatory method
+
+
 def GatherImagesFromDirectory(directory):
     # define supproted types
     types = ['*.png', '*.bmp', '*.jpg']
@@ -13,20 +16,21 @@ def GatherImagesFromDirectory(directory):
     # check if directory exist
     if not os.path.exists(directory):
         print('Directory doesn\'t exist, return empty image path array')
-        return imageList #exit method here
+        return imageList  # exit method here
     # gather images by types
     for type in types:
-        print('Gathering type: ' + type)
-        specificTypeImages = glob.glob(directory + type)
+        specific_type_images = glob.glob(directory + type)
         # add one type images list to main array
-        imageList.extend(specificTypeImages)
+        imageList.extend(specific_type_images)
     imageList.sort()
     return imageList
+
 
 def GetFileName(path):
     name_with_extension = os.path.basename(path)
     name = os.path.splitext(name_with_extension)[0]
     return name
+
 
 def CreateDirectory(directory):
     if not os.path.exists(directory):
@@ -45,13 +49,45 @@ def GetLabel(labels_txt_path, image_name):
         if image_name == name:
             major_axis = float(words[1])
             minor_axis = float(words[2])
-            rotation = float(words[3]) #rad
+            rotation = float(words[3])  # rad
             center_x = float(words[4])
             center_y = float(words[5])
             labels_txt.close()
             return major_axis, minor_axis, rotation, center_x, center_y
     labels_txt.close()
-    return 0.0,0.0,0.0,0.0,0.0
+    return 0.0, 0.0, 0.0, 0.0, 0.0
+
+
+def Check_what_kind_of_label(labels_txt_path, image_name):
+    labels_txt = open(labels_txt_path, "r")
+    lines = labels_txt.readlines()
+    for line in lines:
+        words = line.split()
+        name = words[0]
+        if image_name == name:
+            if int(words[1]) == 1:
+                defektuotas = 1
+                labels_txt.close()
+                return defektuotas
+            if int(words[1]) == 0:
+                nedefektuotas = 0
+                labels_txt.close()
+                return nedefektuotas
+
+
+
+
+
+def Getting_right_file(path, image_name):
+    image_paths = GatherImagesFromDirectory(path)
+    for image_path in image_paths:
+        file_name = GetFileName(image_path)
+        if file_name[0:4] == image_name:
+            file = image_path
+            return file
+        else:
+            continue
+
 
 def positives():
     path_to_positive = r"C:\Users\Urtis\Desktop\Straipsniai\dagm\Class6_def/"
@@ -76,6 +112,7 @@ def positives():
         cv2.imwrite(positive_labels_output_dir + file_name + '.png', label)
         cv2.waitKey(1)
 
+
 def negatives():
     # Negative
     path_to_negative = r"C:\Users\Urtis\Desktop\Straipsniai\dagm\Class6/"
@@ -94,68 +131,138 @@ def negatives():
         cv2.imwrite(negative_labels_output_dir + file_name + '.png', label)
         cv2.waitKey(1)
 
-def Rototion_and_fliping():
-    path_to_photo = r"C:\Users\Urtis\Desktop\Straipsniai\dagm\bandymai/"
-    output_dir = r"C:\Users\Urtis\Desktop\Straipsniai\dagm\pasuktos_nuotraukos/"
-    CreateDirectory(output_dir)
+
+def Rototion_and_fliping_and_lable_making():
+    kintamasis1 = input("Enter --Class-- name for augmntation: ")
+    kintamasis2 = input("Enter is this gona be --Test-- or --Train-- data:")
+    path_to_photo = r"C:\Users\Urtis\Desktop\Straipsniai\dagm\Nauja\DAGM_KaggleUpload\{}".format(kintamasis1) + '\{}'.format(kintamasis2) + '//'
+    path_to_labels = r"C:\Users\Urtis\Desktop\Straipsniai\dagm\Nauja\DAGM_KaggleUpload\{}".format(kintamasis1) + '\{}'.format(kintamasis2) + '\Label/'
+    #####################
+    linkas = r"C:\Users\Urtis\Desktop\Straipsniai\dagm\Nauja\Sugeneruota_baze\{}".format(kintamasis1) + '//'
+    CreateDirectory(linkas)
+    ####################
+    output_image_dir = r"C:\Users\Urtis\Desktop\Straipsniai\dagm\Nauja\Sugeneruota_baze\{}".format(kintamasis1) + '\{}'.format(kintamasis2) + '/'
+    CreateDirectory(output_image_dir)
+    output_labels_dir = r"C:\Users\Urtis\Desktop\Straipsniai\dagm\Nauja\Sugeneruota_baze\{}".format(kintamasis1) + '\{}'.format(kintamasis2) + '\Label/'
+    CreateDirectory(output_labels_dir)
+    labels_txt = path_to_labels + 'Labels.txt'
     # collect all image from directory
     image_paths = GatherImagesFromDirectory(path_to_photo)
-    file_name = 0
+    file_name = 27600  ######################################################### reikia pakeisti kai irasoma nauja Class paskutiniu image NR.
     for image_path in image_paths:
+        file_name1 = GetFileName(image_path)  # gaunamas imago pavadinimas
+        ar_defektuotas = Check_what_kind_of_label(labels_txt, file_name1)
 
-        image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-        file_name = int(file_name) + 1
-        cv2.imwrite(output_dir + str(file_name) + '.png', image)
+        if ar_defektuotas == 0:
+            image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+            file_name = int(file_name) + 1
+            cv2.imwrite(output_image_dir + str(file_name) + '.png', image)
+            height, width = image.shape[:2]
+            label = np.zeros((height, width), np.uint8)
+            cv2.imwrite(output_labels_dir + str(file_name) + '.png', label)
 
-        new_img1 = cv2.rotate(image,rotateCode=0)   # 0 = 90 degrees; 1 = 180 degrees; 2 = 270 degrees
-        file_name = int(file_name) + 1
-        cv2.imwrite(output_dir + str(file_name) + '.png', new_img1)
+            new_img1 = cv2.rotate(image, rotateCode=0)  # 0 = 90 degrees; 1 = 180 degrees; 2 = 270 degrees
+            file_name = int(file_name) + 1
+            cv2.imwrite(output_image_dir + str(file_name) + '.png', new_img1)
+            height1, width1 = new_img1.shape[:2]
+            label1 = np.zeros((height1, width1), np.uint8)
+            cv2.imwrite(output_labels_dir + str(file_name) + '.png', label1)
 
-        new_img2 = cv2.rotate(image, rotateCode=1)  # 0 = 90 degrees; 1 = 180 degrees; 2 = 270 degrees
-        file_name = int(file_name) + 1
-        cv2.imwrite(output_dir + str(file_name) + '.png', new_img2)
+            new_img2 = cv2.rotate(image, rotateCode=1)  # 0 = 90 degrees; 1 = 180 degrees; 2 = 270 degrees
+            file_name = int(file_name) + 1
+            cv2.imwrite(output_image_dir + str(file_name) + '.png', new_img2)
+            height2, width2 = new_img2.shape[:2]
+            label2 = np.zeros((height2, width2), np.uint8)
+            cv2.imwrite(output_labels_dir + str(file_name) + '.png', label2)
 
-        new_img3 = cv2.rotate(image, rotateCode=2)  # 0 = 90 degrees; 1 = 180 degrees; 2 = 270 degrees
-        file_name = int(file_name) + 1
-        cv2.imwrite(output_dir + str(file_name) + '.png', new_img3)
+            new_img3 = cv2.rotate(image, rotateCode=2)  # 0 = 90 degrees; 1 = 180 degrees; 2 = 270 degrees
+            file_name = int(file_name) + 1
+            cv2.imwrite(output_image_dir + str(file_name) + '.png', new_img3)
+            height3, width3 = new_img3.shape[:2]
+            label3 = np.zeros((height3, width3), np.uint8)
+            cv2.imwrite(output_labels_dir + str(file_name) + '.png', label3)
 
-        new_fliped_img4= cv2.flip(image,1)
-        file_name = int(file_name) + 1
-        cv2.imwrite(output_dir + str(file_name) + '.png', new_fliped_img4)
+            new_fliped_img4 = cv2.flip(image, 1)
+            file_name = int(file_name) + 1
+            cv2.imwrite(output_image_dir + str(file_name) + '.png', new_fliped_img4)
+            height4, width4 = new_fliped_img4.shape[:2]
+            label4 = np.zeros((height4, width4), np.uint8)
+            cv2.imwrite(output_labels_dir + str(file_name) + '.png', label4)
 
-        new_img5 = cv2.rotate(new_fliped_img4, rotateCode=0)  # 0 = 90 degrees; 1 = 180 degrees; 2 = 270 degrees
-        file_name = int(file_name) + 1
-        cv2.imwrite(output_dir + str(file_name) + '.png', new_img5)
+            new_img5 = cv2.rotate(new_fliped_img4, rotateCode=0)  # 0 = 90 degrees; 1 = 180 degrees; 2 = 270 degrees
+            file_name = int(file_name) + 1
+            cv2.imwrite(output_image_dir + str(file_name) + '.png', new_img5)
+            height5, width5 = new_img5.shape[:2]
+            label5 = np.zeros((height5, width5), np.uint8)
+            cv2.imwrite(output_labels_dir + str(file_name) + '.png', label5)
 
-        new_img6 = cv2.rotate(new_fliped_img4, rotateCode=1)  # 0 = 90 degrees; 1 = 180 degrees; 2 = 270 degrees
-        file_name = int(file_name) + 1
-        cv2.imwrite(output_dir + str(file_name) + '.png', new_img6)
+            new_img6 = cv2.rotate(new_fliped_img4, rotateCode=1)  # 0 = 90 degrees; 1 = 180 degrees; 2 = 270 degrees
+            file_name = int(file_name) + 1
+            cv2.imwrite(output_image_dir + str(file_name) + '.png', new_img6)
+            height6, width6 = new_img6.shape[:2]
+            label6 = np.zeros((height6, width6), np.uint8)
+            cv2.imwrite(output_labels_dir + str(file_name) + '.png', label6)
 
-        new_img7 = cv2.rotate(new_fliped_img4, rotateCode=2)  # 0 = 90 degrees; 1 = 180 degrees; 2 = 270 degrees
-        file_name = int(file_name) + 1
-        cv2.imwrite(output_dir + str(file_name) + '.png', new_img7)
+            new_img7 = cv2.rotate(new_fliped_img4, rotateCode=2)  # 0 = 90 degrees; 1 = 180 degrees; 2 = 270 degrees
+            file_name = int(file_name) + 1
+            cv2.imwrite(output_image_dir + str(file_name) + '.png', new_img7)
+            height7, width7 = new_img7.shape[:2]
+            label7 = np.zeros((height7, width7), np.uint8)
+            cv2.imwrite(output_labels_dir + str(file_name) + '.png', label7)
 
-        cv2.imshow('image1', image)
-        cv2.waitKey(0)
-        cv2.imshow('image1', new_img1)
-        cv2.waitKey(0)
-        cv2.imshow('image2', new_img2)
-        cv2.waitKey(0)
-        cv2.imshow('image3', new_img3)
-        cv2.waitKey(0)
-        cv2.imshow('image4', new_fliped_img4)
-        cv2.waitKey(0)
-        cv2.imshow('image5', new_img5)
-        cv2.waitKey(0)
-        cv2.imshow('image6', new_img6)
-        cv2.waitKey(0)
-        cv2.imshow('image7', new_img7)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+        if ar_defektuotas == 1:
+
+            image_d = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+            file_name = int(file_name) + 1
+            cv2.imwrite(output_image_dir + str(file_name) + '.png', image_d)
+            label_d = cv2.imread(Getting_right_file(path_to_labels, file_name1))
+            cv2.imwrite(output_labels_dir + str(file_name) + '_label.png', label_d)
+
+            new_img1_d = cv2.rotate(image_d, rotateCode=0)  # 0 = 90 degrees; 1 = 180 degrees; 2 = 270 degrees
+            file_name = int(file_name) + 1
+            cv2.imwrite(output_image_dir + str(file_name) + '.png', new_img1_d)
+            label1_d = cv2.rotate(label_d, rotateCode=0)
+            cv2.imwrite(output_labels_dir + str(file_name) + '_label.png', label1_d)
+
+            new_img2_d = cv2.rotate(image_d, rotateCode=1)  # 0 = 90 degrees; 1 = 180 degrees; 2 = 270 degrees
+            file_name = int(file_name) + 1
+            cv2.imwrite(output_image_dir + str(file_name) + '.png', new_img2_d)
+            label2_d = cv2.rotate(label_d, rotateCode=1)
+            cv2.imwrite(output_labels_dir + str(file_name) + '_label.png', label2_d)
+
+            new_img3_d = cv2.rotate(image_d, rotateCode=2)  # 0 = 90 degrees; 1 = 180 degrees; 2 = 270 degrees
+            file_name = int(file_name) + 1
+            cv2.imwrite(output_image_dir + str(file_name) + '.png', new_img3_d)
+            label3_d = cv2.rotate(label_d, rotateCode=2)
+            cv2.imwrite(output_labels_dir + str(file_name) + '_label.png', label3_d)
+
+            new_fliped_img4_d = cv2.flip(image_d, 1)
+            file_name = int(file_name) + 1
+            cv2.imwrite(output_image_dir + str(file_name) + '.png', new_fliped_img4_d)
+            label4_d = cv2.flip(label_d, 1)
+            cv2.imwrite(output_labels_dir + str(file_name) + '_label.png', label4_d)
+
+            new_img5_d = cv2.rotate(image_d, rotateCode=0)  # 0 = 90 degrees; 1 = 180 degrees; 2 = 270 degrees
+            file_name = int(file_name) + 1
+            cv2.imwrite(output_image_dir + str(file_name) + '.png', new_img5_d)
+            label5_d = cv2.rotate(label_d, rotateCode=0)
+            cv2.imwrite(output_labels_dir + str(file_name) + '_label.png', label5_d)
+
+            new_img6_d = cv2.rotate(image_d, rotateCode=1)  # 0 = 90 degrees; 1 = 180 degrees; 2 = 270 degrees
+            file_name = int(file_name) + 1
+            cv2.imwrite(output_image_dir + str(file_name) + '.png', new_img6_d)
+            label6_d = cv2.rotate(label_d, rotateCode=1)
+            cv2.imwrite(output_labels_dir + str(file_name) + '_label.png', label6_d)
+
+            new_img7_d = cv2.rotate(image_d, rotateCode=2)  # 0 = 90 degrees; 1 = 180 degrees; 2 = 270 degrees
+            file_name = int(file_name) + 1
+            cv2.imwrite(output_image_dir + str(file_name) + '.png', new_img7_d)
+            label7_d = cv2.rotate(label_d, rotateCode=2)
+            cv2.imwrite(output_labels_dir + str(file_name) + '_label.png', label7_d)
+
 
 def main():
-    Rototion_and_fliping()
-
+    Rototion_and_fliping_and_lable_making()
 
 
 # what we will start? (entry point)
